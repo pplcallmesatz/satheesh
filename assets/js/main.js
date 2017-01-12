cb5a56bab119179bc42a00c6cd75bc7599c6aba4
Satheesh Kumar <satheeshssk@icloud.com>
2017-01-12 11:30:27 +0530

    Added date function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,13 @@ $(function() {
       
     if (usrCnt.data.length > 0) {							
       $.each(usrCnt.data.reverse(), function(i, val) {
+          
+          var spli = val.created_at;
+          var datee = spli.split('T');
+          var uploadDate = datee[0];
+        
         $('#dribbble').prepend(
-          '<div class="box col-md-4"><a href="'+val.html_url+'" target="_blank"><img src="'+val.images.normal+'" /></a><h5 class="text-center">'+val.title+'</h5><span><i class="fa fa-eye" aria-hidden="true"></i> '+val.views_count+'</span><span> <i class="fa fa-heart" aria-hidden="true"></i> '+val.likes_count+'</span> <span> <i class="fa fa-comment" aria-hidden="true"></i> '+val.comments_count+'</span></div>'
+          '<div class="box col-md-4"><a href="'+val.html_url+'" target="_blank"><img src="'+val.images.normal+'" /></a><h5 class="text-center">'+val.title+'</h5><span><i class="fa fa-eye" aria-hidden="true"></i> '+val.views_count+'</span><span> <i class="fa fa-heart" aria-hidden="true"></i> '+val.likes_count+'</span> <span> <i class="fa fa-comment" aria-hidden="true"></i> '+val.comments_count+'</span><span> <i class="fa fa-calendar" aria-hidden="true"></i>'+ uploadDate+'</span></div>'
         );
 		  $(".post").prepend(
 		  '<div class="col-md-4 col-sm-6"></div>'
@@ -16,20 +21,9 @@ $(function() {
     }
   });
     $.getJSON('https://api.dribbble.com/v1/user?access_token=aed5bfd4b691546feeccbd0f596814d259ffdfc917e2c48d545de625b9d2b877', function(usrProfile) {
-      console.log(usrProfile.name);
 		var profileName = usrProfile.name;
-		var usrName = usrProfile.username;
+		var usrLocation = usrProfile.location;
 $( ".profile-name" ).html( profileName );
-$( ".user-name" ).html("@" + usrName );
-//    if (resp.data.length > 0) {							
-//      $.each(resp.data.reverse(), function(i, val) {
-//        $('#dribbble').prepend(
-//          '<li class="box"><img src="'+val.images.normal+'" /><h2>'+val.title+'</h2><span>'+val.views_count+'</span><h2>'+val.likes_count+'</h2> <h2>'+val.comments_count+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p></li>'
-//        );
-//      });
-//    }
-//    else {
-//      $('#dribbble').append('<li>No shots.</li>');
-//    }
+$( ".user-name" ).html("@" + usrLocation );
   });		   							    
 });
